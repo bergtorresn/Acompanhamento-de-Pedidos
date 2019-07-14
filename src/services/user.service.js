00c3c98b238b3cc6ade9883a6f4f30c71b398c44
Rosemberg Torres <bergtorres1@gmail.com>
2019-07-13 22:59:29 -0300

    adjustes

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,38 @@
 import axios from 'axios';
-import config from '../config/config';
+
+const baseUrl = "http://localhost:4000/"
+
+function get(apiEndpoint) {
+    return axios.get(baseUrl + apiEndpoint).then((response) => {
+        return response;
+    }).catch((err) => {
+        console.log("***** ERRO: " + err);
+    })
+}
+
+function post(apiEndpoint, payload) {
+    return axios.post(baseUrl + apiEndpoint, payload).then((response) => {
+        return response;
+    }).catch((err) => {
+        console.log("***** ERRO: " + err);
+    })
+}
+
+function put(apiEndpoint, payload) {
+    return axios.put(baseUrl + apiEndpoint, payload).then((response) => {
+        return response;
+    }).catch((err) => {
+        console.log("***** ERRO: " + err);
+    })
+}
+
+function deleteDetail(apiEndpoint) {
+    return axios.delete(baseUrl + apiEndpoint).then((response) => {
+        return response;
+    }).catch((err) => {
+        console.log("***** ERRO: " + err);
+    })
+}
 
 export const requests = {
     get,
@@ -7,33 +40,3 @@ export const requests = {
     put,
     deleteDetail
 };
-
-function get(apiEndpoint) {
-    return axios.get(config.baseUrl + apiEndpoint).then((response) => {
-        return response;
-    }).catch((err) => {
-        console.log(err);
-    })
-}
-
-function post(apiEndpoint, payload) {
-    return axios.post(config.baseUrl + apiEndpoint, payload).then((response) => {
-        return response;
-    }).catch((err) => {
-        console.log(err);
-    })
-}
-function put(apiEndpoint, payload) {
-    return axios.put(config.baseUrl + apiEndpoint, payload).then((response) => {
-        return response;
-    }).catch((err) => {
-        console.log(err);
-    })
-}
-function deleteDetail(apiEndpoint) {
-    return axios.delete(config.baseUrl + apiEndpoint).then((response) => {
-        return response;
-    }).catch((err) => {
-        console.log(err);
-    })
-}
